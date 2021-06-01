c7187534c163ab2f9c45d7178aa23b8bb5f962c5
Olha Baturina <olga.baturina.golovastova@gmail.com>
2021-06-02 01:14:49 +0300

    Добавляет выпадающие ошибки и рендерит список стран

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,9 @@
 import '../css/style.css';
-import '@pnotify/core/dist/BrightTheme.css';
 
+import '@pnotify/core/dist/BrightTheme.css';
+import '@pnotify/core/dist/BrightTheme.css';
 import PNotify, { notice, info, alert, success, error, close } from '@pnotify/core';
+
 import _ from '../../node_modules/lodash/lodash';
 
 import fetchCountries from './fetchCountries';
@@ -11,26 +13,44 @@ import countriesList from '../templates/list-countries.hbs';
 const renderRef = document.querySelector('.js-render');
 const inputRef = document.querySelector('[data-input="searchQuery"]');
 
-inputRef.addEventListener('input', _.debounce(onSearch, 1500));
+inputRef.addEventListener('input', _.debounce(onSearch, 500));
 
 function onSearch(evt) {
   evt.preventDefault();
   const form = evt.target;
   console.log(form.value);
   const searchQuery = form.value.trim();
-  // if ((searchQuery = '')) {
-  //   return;
-  // }
+if (searchQuery === '') {
+    return error('Empty request, enter your search data')
+  }
+
 
   fetchCountries(searchQuery)
-    .then(renderCountry)
+    .then(markup)
     .catch(error => console.log(error));
 }
 
 function renderCountry(country) {
   const cardMarkup = countryCard(country);
-
   renderRef.innerHTML = cardMarkup;
-
-  // const countryListMarkup = countryCard(countriesList);
 }
+
+function renderCountriesList(country) {
+  const cardMarkup = countriesList(country);
+  renderRef.innerHTML = cardMarkup;
+}
+
+function markup(arrayCountries) {
+  if (arrayCountries.length === 1) {
+    renderCountry(arrayCountries);
+  }
+  if (arrayCountries.length > 1 && arrayCountries.length < 10) {
+    notice('Please select a country from the list');
+    renderCountriesList(arrayCountries);
+  }
+  if (arrayCountries.length > 10) {
+    renderRef.innerHTML = '';
+    alert('Too many matches foundю Please enter a more specific query!');
+  }
+}
+
