f8e689364991333696e97b4669fd44eb67f4e03d
Olha Baturina <olga.baturina.golovastova@gmail.com>
2021-06-01 03:24:06 +0300

    Вносит изменения в функции

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,28 @@
 import '../css/style.css';
 import '@pnotify/core/dist/BrightTheme.css';
 
+import PNotify, { notice, info, alert, success, error, close } from '@pnotify/core';
+import _ from '../../node_modules/lodash/lodash';
+
 import fetchCountries from './fetchCountries';
 import countryCard from '../templates/country-card.hbs';
 import countriesList from '../templates/list-countries.hbs';
 
 const renderRef = document.querySelector('.js-render');
-const inputRef = document.querySelector('.input-js');
+const inputRef = document.querySelector('[data-input="searchQuery"]');
 
 let searchQuery = '';
 
-inputRef.addEventListener('submit', onSearch);
+inputRef.addEventListener('input', _.debounce(onSearch, 500));
 
 function onSearch(evt) {
   evt.preventDefault;
-  searchQuery = evt.target.value;
+  const searchQuery = evt.target.value.trim();
+  if ((searchQuery = '')) {
+    return;
+  }
+  //  очищаем старую разметку
+  renderRef.innerHTML('');
 
   fetchCountries(searchQuery)
     .then(renderCountry)
